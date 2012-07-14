3a1d3c8ae7ad7b032d62c79591ed8754c0fe2e27
François Gannaz <francois.gannaz@silecs.info>
2012-07-14 11:47:37 +0200

    Revert "Remove some obsolete profile_* functions"
    This partly reverts commit b2dd00d6c31743c16b81a9df53108a7224495455.
    The old API is left unchanged.

--- user/profile/definelib.php
+++ user/profile/definelib.php
@@ -1,3 +1,70 @@
 <?php
 
 require_once($CFG->libdir . '/custominfo/lib.php');
+
+/**
+ * Reorder the profile fields within a given category starting
+ * at the field at the given startorder
+ */
+function profile_reorder_fields() {
+    return custominfo_field::type('user')->reorder();
+}
+
+/**
+ * Reorder the profile categoriess starting at the category
+ * at the given startorder
+ */
+function profile_reorder_categories() {
+    return custominfo_category::type('user')->reorder();
+}
+
+/**
+ * Delete a profile category
+ * @param   integer   $id id of the category to be deleted
+ * @return  boolean   success of operation
+ */
+function profile_delete_category($id) {
+    return custominfo_category::findById($id)->delete();
+}
+
+
+function profile_delete_field($id) {
+    return custominfo_field::findById($id)->delete();
+}
+
+/**
+ * Change the sortorder of a field
+ * @param   integer   id of the field
+ * @param   string    direction of move
+ * @return  boolean   success of operation
+ */
+function profile_move_field($id, $move) {
+    return custominfo_field::findById($id)->move($move);
+}
+
+/**
+ * Change the sortorder of a category
+ * @param   integer   id of the category
+ * @param   string    direction of move
+ * @return  boolean   success of operation
+ */
+function profile_move_category($id, $move) {
+    return custominfo_category::findById($id)->move($move);
+}
+
+/**
+ * Retrieve a list of all the available data types
+ * @return   array   a list of the datatypes suitable to use in a select statement
+ */
+function profile_list_datatypes() {
+    return custominfo_field::list_datatypes();
+}
+
+/**
+ * Retrieve a list of categories and ids suitable for use in a form
+ * @return   array
+ */
+function profile_list_categories() {
+    return custominfo_category::type('user')->list_assoc();
+}
+
