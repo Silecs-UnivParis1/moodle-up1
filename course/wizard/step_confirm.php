6b520337680ee504da76a321e1b4b2c8c3f09d15
Cécile Guilloux <cecile.guilloux@silecs.info>
2012-07-16 00:01:06 +0200

    wizard/etape comfirm - affichage infos

--- course/wizard/step_confirm.php
+++ course/wizard/step_confirm.php
@@ -15,17 +15,47 @@ class course_wizard_step_confirm extends moodleform {
         $mform->addElement('header','confirmation', 'Confirmation');
 
         $mform->addElement('html', html_writer::tag('p', 'Votre demande d\'ouverture d\'espace de cours va être transmise aux modérateurs de la plateforme :'));
-        $mform->addElement('html', html_writer::tag('p', '[Prénom Nom et/ou membres du service TICE]'));
+        $mform->addElement('html', html_writer::tag('p', '[Prénom Nom du modérateur et/ou membres du service TICE]'));
         $mform->addElement('textarea', 'remarques', 'Vos remarques ou questions concernant cet espace de cours', array('rows'=>15, 'cols'=>80));
         $mform->setType('content', PARAM_RAW);
 
+        $user_name = $USER->firstname . ' '. $USER->lastname;
         $mform->addElement('header','resume', 'Récapitulatif de la demande');
         $mform->addElement('text', 'user_name', 'Nom du demandeur', 'maxlength="40" size="20", disabled="disabled"');
-        $mform->setConstant('user_name', $USER->firstname . ' '. $USER->lastname);
+        $mform->setConstant('user_name', $user_name);
         $tabfreeze[] = 'user_name';
         $mform->addElement('date_selector', 'requestdate', 'Date de la demande de création');
         $mform->setDefault('requestdate', time());
         $tabfreeze[] = 'requestdate';
+
+        $idcat = $SESSION->wizard['form_step2']['category'];
+        $displaylist = array();
+        $parentlist = array();
+        make_categories_list($displaylist, $parentlist);
+        $mform->addElement('select', 'category', get_string('category'), $displaylist);
+        $mform->setConstant('category',$idcat);
+        $tabfreeze[] = 'category';
+
+        $fullname = $SESSION->wizard['form_step2']['fullname'];
+        $mform->addElement('text','fullname', get_string('fullnamecourse'),'maxlength="254" size="50"');
+        $mform->setConstant('fullname', $fullname);
+        $tabfreeze[] = 'fullname';
+
+        $shortname = $SESSION->wizard['form_step2']['shortname'];
+        $mform->addElement('text', 'shortname', get_string('shortnamecourse'), 'maxlength="100" size="20"');
+        $mform->setConstant('shortname', $shortname);
+        $tabfreeze[] = 'shortname';
+
+        $htmlsummary = '<div class="fitemtitle"><div class="fstaticlabel"><label>'
+            . get_string('coursesummary') . '</label></div></div>'
+            . '<div class="felement fstatic">' . $SESSION->wizard['form_step2']['summary_editor']['text'] . '</div>';
+        $mform->addElement('html', html_writer::tag('div', $htmlsummary, array('class' => 'fitem')));
+
+        $date = $SESSION->wizard['form_step2']['startdate'];
+		$startdate = mktime(0, 0, 0, $date['month'], $date['day'], $date['year']);
+		$mform->addElement('date_selector', 'startdate', get_string('startdate'));
+		$mform->setConstant('startdate', $startdate);
+        $tabfreeze[] = 'startdate';
 
         $mform->addElement('hidden', 'stepin', null);
         $mform->setType('stepin', PARAM_INT);
