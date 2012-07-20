83bbf002b128f9a5994565a72c98c1da71dcc7db
Guillaume Allegre <guillaume.allegre@silecs.info>
2012-07-20 16:57:48 +0200

    course synopsis + enrolled cohorts

--- course/report/synopsis/locallib.php
+++ course/report/synopsis/locallib.php
@@ -29,6 +29,33 @@ defined('MOODLE_INTERNAL') || die;
 require_once(dirname(__FILE__).'/lib.php');
 require_once($CFG->dirroot.'/course/lib.php');
 
+/**
+ * Returns list of cohorts enrolled into course/context.
+ * @todo this function should be moved to  enrol/cohort/locallib.php
+ * OR lib/accesslib.php (next to get_enrolled_users)
+ *
+ * @param context $context (optional)
+ * @param course id $courseid (optional)
+ * @param array(role_id, ...) $roleids
+ * @return array of cohort records
+ */
+
+function get_enrolled_cohorts($courseid, $roleids=null) {
+    global $DB;
+
+	    $sql = "SELECT c.id, c.name, c.idnumber, c.description
+              FROM {cohort} c
+              JOIN {enrol} e ON (e.enrol='cohort' AND e.customint1=c.id) ";
+//			  JOIN {role} r ON (r.id = e.roleid) //** @todo bugfix DML read exception; don't know why
+		$sql .= " WHERE e.courseid = ? ";
+	if ( isset($roleids) ) {
+		$sql .= "AND roleid IN (". implode(',', $roleids) .")";
+	}
+	$sql .= " ORDER BY c.name ASC";
+
+    return $DB->get_records_sql($sql, array($courseid));
+}
+
 
 function report_outline_print_row($mod, $instance, $result) {
     global $OUTPUT, $CFG;
--- course/report/synopsis/mod.php
+++ course/report/synopsis/mod.php
@@ -25,4 +25,3 @@
 
 $url = new moodle_url('/course/report/synopsis/index.php', array('id'=>$id));
 echo '<a href="'. $url .'">' . get_string('Synopsis', 'coursereport_synopsis') . '</a>';
-
