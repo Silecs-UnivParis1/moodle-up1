8b349dc9e4ad6e3302856c16d9e7c422361b29b3
Guillaume Allegre <guillaume.allegre@silecs.info>
2012-07-23 15:52:26 +0200

    + table user_sync

--- auth/ldapup1/db/install.php
+++ auth/ldapup1/db/install.php
@@ -1,6 +1,20 @@
 <?php
 
+/**
+ * Post installation and migration code.
+ *
+ * @package    auth
+ * @subpackage ldapup1
+ * @copyright  2012 Silecs {@link http://www.silecs.info/societe}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
 function xmldb_auth_ldapup1_install() {
     global $CFG, $DB;
 
 }
+
+// insert into user_sync(id, ref_plugin, timemodified) select id, auth, unix_timestamp() from user;
+// update user_sync set ref_plugin='auth_ldapup1' where ref_plugin='ldap' ;
--- auth/ldapup1/db/upgrade.php
+++ auth/ldapup1/db/upgrade.php
@@ -1,6 +1,21 @@
-<?php
+/**
+ * Plugin upgrade code.
+ *
+ * @package    auth
+ * @subpackage ldapup1
+ * @copyright  2012 Silecs {@link http://www.silecs.info/societe}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 
-function xmldb_auth_ldapup1_install() {
+defined('MOODLE_INTERNAL') || die;
+
+function xmldb_auth_ldapup1_upgrade($oldversion) {
     global $CFG, $DB;
 
+    $dbman = $DB->get_manager();
+
+    // Moodle v2.3.0 release upgrade line
+    // Put any upgrade step following this
+
+    return true;
 }
