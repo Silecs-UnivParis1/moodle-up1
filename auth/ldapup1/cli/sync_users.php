83677978948c4d0e283670e9f64a0752c33b0871
Guillaume Allegre <guillaume.allegre@silecs.info>
2012-07-23 18:18:28 +0200

    (m) ldapup1 cleaned doc comments

--- auth/ldapup1/cli/sync_users.php
+++ auth/ldapup1/cli/sync_users.php
@@ -22,7 +22,7 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  * derived from official auth_ldap
  *
- * CAS user sync script.
+ * user sync script.
  *
  * This script is meant to be called from a cronjob to sync moodle with the LDAP
  * backend in those setups where the LDAP backend acts as 'master'.
@@ -33,12 +33,6 @@
  *
  * Notes:
  *   - it is required to use the web server account when executing PHP CLI scripts
- *   - you need to change the "www-data" to match the apache user account
- *   - use "su" if "sudo" not available
- *   - If you have a large number of users, you may want to raise the memory limits
- *     by passing -d momory_limit=256M
- *   - For debugging & better logging, you are encouraged to use in the command line:
- *     -d log_errors=1 -d error_reporting=E_ALL -d display_errors=0 -d html_errors=0
  *   - If you have a large number of users, you may want to raise the memory limits
  *     by passing -d momory_limit=256M
  *   - For debugging & better logging, you are encouraged to use in the command line:
