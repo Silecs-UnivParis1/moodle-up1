c16dc7c77dc3fb6d1e85ad858c75c15a3b8c2b25
Guillaume Allegre <guillaume.allegre@silecs.info>
2012-07-27 15:21:46 +0200

    + theme leatherbound-up1

---
+++ theme/leatherbound-up1/lang/fr/theme_leatherbound-up1.php
@@ -0,0 +1,34 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'theme_leatherbound', language 'en', branch 'MOODLE_20_STABLE'
+ *
+ * @package   theme
+ * @subpackage leatherbound-up1
+ * @copyright 2012 Université Paris 1 (UP1), France
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'Leatherbound UP1';
+$string['region-side-post'] = 'Droite';
+$string['region-side-pre'] = 'Gauche';
+$string['choosereadme'] = '<div class="clearfix"><div class="theme_screenshot"><h2>Leatherbound UP1</h2><img src="leatherbound-up1/pix/screenshot.jpg" />'
+. '<h2>À propos</h2><p>Leatherbound est un thème à largeur variable et à trois colonnes pour Moodle 2.0, créé par Patrick Malley. Leatherbound UP1 est un travail dérivé.'
+. '<h2>Tweaks</h2><p>Ce thème est construit sur Base et Canvas, deux thèmes parents, inclus dans Moodle core.</p>'
+. '<h2>Crédits</h2><p>Leatherbound (original) est écrit et maintenu par Patrick Malley de NewSchool Learning. Il peut être contacté à contact@newschoollearning.com. </p>. <p>Leatherbound UP1 est une variante de l\' Université Paris 1 (France).</p>'
+. '<h2>Licence</h2><p>Ce travail et les autres thèmes inclus dans Moodle core sont placés sous la licence <a href="http://www.gnu.org/licenses/gpl.html">GNU General Public License</a>.</div></div>';
