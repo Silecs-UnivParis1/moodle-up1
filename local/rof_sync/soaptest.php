bb665d5035ced2873e3b5c732389eb4fec82238a
Guillaume Allegre <guillaume.allegre@silecs.info>
2012-07-30 16:31:23 +0200

    code de débogage 1

--- local/rof_sync/soaptest.php
+++ local/rof_sync/soaptest.php
@@ -17,20 +17,44 @@ echo "\nTypes: ";
 $types = $soapClient->__getTypes();
 print_r($types);
 
-
+echo "\n\n**************\n\n";
 
 //list of formations
 $url = $urlbase . $urlext . '?wsdl' ;
-$soapClient = new SoapClient($url);
+$soapClient = new SoapClient($url, array('trace' => 1));
 
+/*
 $requestParams = array(
     '_cmd' => 'getAllFormations',
     '_lang' => 'fr-FR',
     );
+*/
+$requestParams = array(
+    '_cmd' => 'getFormation',
+    '_lang' => 'fr-FR',
+    '_oid' => 'UP1-PROG34252',
+    );
 
-$formResponse = $soapClient->__soapCall('getResponse', $requestParams);
+try {
+    // $formResponse = $soapClient->__soapCall('getParametersBean', $requestParams);
+    $formResponse = $soapClient->__soapCall('getResponse', $requestParams);
+} catch (SoapFault $soapFault) {
+    echo "SoapFault : \n";
+    echo $soapFault;
+    echo "\nFin SoapFault\n\n" ;
+}
+
+$handle = fopen("lastrequest.xml", "w");
+fwrite ($handle, $soapClient->__getLastRequest());
+// echo "Request : \n", $soapClient->__getLastRequest(), "\n";
+fclose($handle);
+$handle = fopen("lastresponse.xml", "w");
+fwrite ($handle, $soapClient->__getLastResponse());
+// echo "Response : \n", $soapClient->__getLastResponse(), "\n";
+fclose($handle);
+
 
 // $formResponse = $soapClient->getResponse($requestParams);
 
 
-var_dump($formResponse);
+//var_dump($formResponse);
