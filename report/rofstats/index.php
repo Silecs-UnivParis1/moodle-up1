021d1554cedb8b0511772881bd66942d8559862e
Guillaume Allegre <guillaume.allegre@silecs.info>
2012-08-30 16:30:46 +0200

    rofstats page stats

--- report/rofstats/index.php
+++ report/rofstats/index.php
@@ -19,8 +19,6 @@ require_login();
 
 // $issue = optional_param('issue', '', PARAM_ALPHANUMEXT); // show detailed info about one issue only
 
-
-
 // Print the header.
 admin_externalpage_setup('reportrofstats', '', null, '', array('pagelayout'=>'report'));
 echo $OUTPUT->header();
@@ -30,8 +28,25 @@ echo $OUTPUT->heading(get_string('pluginname', 'report_rofstats'));
 $url = "$CFG->wwwroot/report/rofstats/index.php";
 
 
+echo "<h1>Counts</h1>\n";
+$table = new html_table();
+$table->head = array('Items', 'Nb');
+$table->data = report_rofstats_generic();
+echo html_writer::table($table);
 
-    $table = new html_table();
+echo "<h1>Components</h1>\n";
+$table = new html_table();
+$table->head = array('', '# Programs', 'ROFid', 'Name');
+$table->data = report_rofstats_components();
+echo html_writer::table($table);
+
+echo "<h1>Persons</h1>\n";
+$table = new html_table();
+$table->head = array('Levels', 'Persons not empty');
+$table->data = report_rofstats_persons_not_empty();
+echo html_writer::table($table);
+
+
 /*    $table->head  = array($strissue, $strstatus, $strdesc, $strconfig);
     $table->size  = array('30%', '10%', '50%', '10%' );
     $table->align = array('left', 'left', 'left', 'left');
@@ -41,13 +56,11 @@ $url = "$CFG->wwwroot/report/rofstats/index.php";
     $table->data[] = $row;
 */
 
-    echo html_writer::table($table);
+
 
     // echo $OUTPUT->box($result->details, 'generalbox boxwidthnormal boxaligncenter'); // TODO: add proper css
 
     // echo $OUTPUT->continue_button($url);
-
-    echo html_writer::table($table);
 
 
 echo $OUTPUT->footer();
--- report/rofstats/locallib.php
+++ report/rofstats/locallib.php
@@ -11,7 +11,68 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-function report_rofstats_foobar() {
-     global $DB;
+function report_rofstats_generic() {
+    global $DB;
+    $res = array();
+
+    $count = $DB->count_records('rof_component');
+    $res[] = array('Components', $count);
+    $res[] = array('', ''); //** @todo meilleur séparateur ?
+    $count = $DB->count_records('rof_program', array('level' => 1));
+    $res[] = array('Programs', $count);
+    $count = $DB->count_records('rof_program', array('level' => 2));
+    $res[] = array('SubPrograms', $count);
+    $res[] = array('', ''); //** @todo meilleur séparateur ?
+    $count = $DB->count_records('rof_person');
+    $res[] = array('Persons', $count);
+    $res[] = array('', ''); //** @todo meilleur séparateur ?
+
+    $levelmax = $DB->get_record_sql('SELECT MAX(level) as levelmax FROM {rof_course} rc')->levelmax;
+    for ($level = 1; $level <= 1+$levelmax ; $level++) {
+       $count = $DB->count_records('rof_course', array('level' => $level));
+       $res[] = array('Courses level=' . $level, $count);
+    }
+    return $res;
 }
 
+function report_rofstats_components() {
+    global $DB;
+    $res = array();
+
+    $progsmax = $DB->get_record_sql('SELECT MAX(subnb) as progsmax FROM {rof_component}')->progsmax;
+    $progsmin = $DB->get_record_sql('SELECT MIN(subnb) as progsmin FROM {rof_component} WHERE subnb>0')->progsmin;
+
+    $components = $DB->get_records('rof_component', array('subnb' => $progsmax));
+    foreach ($components as $component) {
+        $res[] = array('Max', $progsmax, $component->number, $component->name);
+    }
+    $components = $DB->get_records('rof_component', array('subnb' => $progsmin));
+    foreach ($components as $component) {
+        $res[] = array('Min > 0', $progsmin, $component->number, $component->name);
+    }
+    $components = $DB->get_records('rof_component', array('subnb' => 0));
+    foreach ($components as $component) {
+        $res[] = array('None', '0', $component->number, $component->name);
+    }
+    return $res;
+}
+
+
+function report_rofstats_persons_not_empty() {
+    global $DB;
+    $res = array();
+
+    $count = $DB->count_records_sql("SELECT COUNT(id) FROM {rof_program} WHERE level=1 AND refperson != ''");
+    $res[] = array('Programs', $count);
+    $count = $DB->count_records_sql("SELECT COUNT(id) FROM {rof_program} WHERE level=2 AND refperson != ''");
+    $res[] = array('SubPrograms', $count);
+    $res[] = array('', ''); //** @todo meilleur séparateur ?
+
+    $levelmax = $DB->get_record_sql('SELECT MAX(level) as levelmax FROM {rof_course} rc')->levelmax;
+    for ($level = 1; $level <= $levelmax ; $level++) {
+       $count = $DB->count_records_sql(
+               "SELECT COUNT(id) FROM {rof_course} WHERE level=? AND refperson != ''", array($level));
+       $res[] = array('Courses level=' . $level, $count);
+    }
+    return $res;
+}
