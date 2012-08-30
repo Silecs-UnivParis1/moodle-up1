bd4fc2371d385ff609988169f110a80720cbd915
Guillaume Allegre <guillaume.allegre@silecs.info>
2012-08-30 18:56:30 +0200

    rofstats + lien rof_browser

--- report/rofstats/index.php
+++ report/rofstats/index.php
@@ -27,40 +27,36 @@ echo $OUTPUT->heading(get_string('pluginname', 'report_rofstats'));
 
 $url = "$CFG->wwwroot/report/rofstats/index.php";
 
+$browserurl = "$CFG->wwwroot/local/rof_browser/rof_browser.php";
+echo '<div><a href="' . $browserurl. '">ROF browser</a></div>';
 
-echo "<h1>Counts</h1>\n";
+
+echo "<h3>Counts</h3>\n";
 $table = new html_table();
 $table->head = array('Items', 'Nb');
 $table->data = report_rofstats_generic();
 echo html_writer::table($table);
 
-echo "<h1>Components</h1>\n";
+echo "<h3>Components</h3>\n";
 $table = new html_table();
 $table->head = array('', '# Programs', 'ROFid', 'Name');
 $table->data = report_rofstats_components();
 echo html_writer::table($table);
 
-echo "<h1>Persons</h1>\n";
+echo "<h3>Persons</h3>\n";
 $table = new html_table();
 $table->head = array('Levels', 'Persons not empty');
 $table->data = report_rofstats_persons_not_empty();
 echo html_writer::table($table);
 
-
-/*    $table->head  = array($strissue, $strstatus, $strdesc, $strconfig);
+/*  $table->head  = array($strissue, $strstatus, $strdesc, $strconfig);
     $table->size  = array('30%', '10%', '50%', '10%' );
     $table->align = array('left', 'left', 'left', 'left');
     $table->attributes = array('class'=>'scurityreporttable generaltable');
     $table->data  = array();
-
     $table->data[] = $row;
 */
 
-
-
-    // echo $OUTPUT->box($result->details, 'generalbox boxwidthnormal boxaligncenter'); // TODO: add proper css
-
-    // echo $OUTPUT->continue_button($url);
-
+echo '<div><a href="' . $browserurl. '">ROF browser</a></div>';
 
 echo $OUTPUT->footer();
