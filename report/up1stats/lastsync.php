7979720bd176025ffe1a6c24dc9cf1041f01ef8b
Guillaume Allegre <guillaume.allegre@silecs.info>
2012-09-06 12:09:01 +0200

    up1stats + lastsync independant page

---
+++ report/up1stats/lastsync.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * UP1 Users Statistics - Top NN cohorts page
+ *
+ * @package    report
+ * @subpackage up1stats
+ * @copyright  2012 Silecs {@link http://www.silecs.info/societe}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+define('NO_OUTPUT_BUFFERING', true);
+
+require('../../config.php');
+require_once($CFG->dirroot.'/report/up1stats/locallib.php');
+require_once($CFG->libdir.'/adminlib.php');
+
+require_login();
+
+$howmany = optional_param('number', 50, PARAM_INT);
+
+// Print the header.
+admin_externalpage_setup('reportup1stats', '', null, '', array('pagelayout'=>'report'));
+echo $OUTPUT->header();
+
+echo $OUTPUT->heading('Cohortes - top '.$howmany);
+
+$url = "$CFG->wwwroot/report/up1stats/index.php";
+
+echo "<h3>Cohortes - top $howmany</h3>\n";
+$table = new html_table();
+$table->head = array('Effectif', 'Nom', 'Id');
+$table->data = report_up1stats_cohorts_top($howmany);
+echo html_writer::table($table);
+
+echo $OUTPUT->footer();
