66ce0cd73616e903291482814733cc7465f41cec
François Gannaz <francois.gannaz@silecs.info>
2012-10-08 01:30:28 +0200

    CourseWizard: simplify get_stepgo()

--- course/wizard/lib_wizard.php
+++ course/wizard/lib_wizard.php
@@ -1,30 +1,20 @@
 <?php
 
 function get_stepgo($stepin, $post) {
+    $stepgo = $stepin + 1;
     switch ($stepin) {
-        case 1:
-            $stepgo = 2;
-            break;
         case 2:
-            $stepgo = 3;
             if (array_key_exists('stepgo_1', $post)) {
                 $stepgo = 1;
             }
             break;
         case 3:
-            $stepgo = 4;
             if (array_key_exists('stepgo_2', $post)) {
                 $stepgo = 2;
             }
             break;
         case 5:
             $stepgo = $post['stepgo'];
-            break;
-        case 6:
-            $stepgo = 7;
-            break;
-        case 7:
-            $stepgo = 8;
             break;
     }
     return $stepgo;
