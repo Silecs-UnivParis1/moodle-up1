4c43154bbbff692b8f6010fe41bc6bf99b413af0
Guillaume Allegre <guillaume.allegre@silecs.info>
2012-10-09 18:09:31 +0200

    local_courseboard  effectif

---
+++ local/courseboard/version.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Version info
+ *
+ * @package    local
+ * @subpackage courseboard
+ * @copyright  2012 Silecs {@link http://www.silecs.info/societe}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+$plugin->version   = 2012100801;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->requires  = 2012061700;        // Requires this Moodle version
+$plugin->component = 'local_courseboard'; // Full name of the plugin (used for diagnostics)
