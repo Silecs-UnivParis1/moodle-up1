e72321ab38ee2d6d22a8f228f861a03792238a28
Guillaume Allegre <guillaume.allegre@silecs.info>
2012-10-12 10:53:05 +0200

    courseboard basé sur id, plus sur idnumber

--- local/courseboard/view.php
+++ local/courseboard/view.php
@@ -8,35 +8,40 @@ require_once($CFG->libdir.'/adminlib.php');
 require_once($CFG->dirroot.'/course/report/synopsis/locallib.php');
 
 require_login();
-$idnumber = required_param('idnumber', PARAM_ALPHANUMEXT); //code Apogée
+// $idnumber = required_param('idnumber', PARAM_ALPHANUMEXT); //code Apogée
+$id = required_param('id', PARAM_INT);  // course id
 
-$course = $DB->get_record('course', array('idnumber'=>$idnumber), '*', MUST_EXIST);
+$course = $DB->get_record('course', array('id' => $id), '*', MUST_EXIST);
 $context_sys = get_context_instance(CONTEXT_SYSTEM);
 require_capability('moodle/site:approvecourse', $context_sys); //** @todo trouver une meilleure capacité
 
 $PAGE->set_context($context_sys);
-$PAGE->set_url('/local/courseboard/view.php', array('idnumber' => $idnumber));
+$PAGE->set_url('/local/courseboard/view.php', array('id' => $id));
 $PAGE->set_pagelayout('course'); //** @todo nécessaire ici ?
 $PAGE->set_title($course->shortname .': '. "tableau de bord");
 $PAGE->set_heading($course->fullname);
 
 echo $OUTPUT->header();
-echo $OUTPUT->heading('Tableau de bord ' . $idnumber );
+echo $OUTPUT->heading('Tableau de bord ' . $course->shortname );
 
-$url = "$CFG->wwwroot/local/courseboard/view.php";
+// $url = "$CFG->wwwroot/local/courseboard/view.php";
 // $browserurl = "$CFG->wwwroot/local/rof_browser/rof_browser.php";
 
 echo "<h2>Description</h2>\n";
 echo "<ul>\n";
 echo "<li>Abrégé : ". $course->shortname ."</li>\n";
-echo "<li>Code : ". $course->idnumber ."</li>\n";
+echo "<li>Code Apogée : ". ( ! empty($course->idnumber) ? $course->idnumber : '<b>NON renseigné</b>') ."</li>\n";
 echo "</ul>\n";
 
 // custom info data
 html_custom_data($course);
 
 // ROF data
-    $rofcourse = $DB->get_record('rof_course', array('code' => $idnumber));
+    if ( ! empty($course->idnumber) ) {
+        $rofcourse = $DB->get_record('rof_course', array('code' => $course->idnumber));
+    } else {
+        $rofcourse = FALSE;
+    }
 
     if ( $rofcourse ) {
         $rofid = $rofcourse->rofid;
@@ -53,7 +58,7 @@ html_custom_data($course);
         echo '</ol>';
     } else {
         echo "<h3>Pas de ROF</h3>\n";
-        echo "<p>Aucun cours correspondant n'existe dans le ROF avec ce code <b>$idnumber</b>.</p>\n";
+        echo "<p>Aucun cours correspondant n'existe dans le ROF avec ce code <b>$course->idnumber</b>.</p>\n";
     }
 
 
