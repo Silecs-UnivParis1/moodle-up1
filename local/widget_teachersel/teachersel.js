fd7cb9214a3365fa6ae7685872ec0920f72b4766
Cécile Guilloux <cecile.guilloux@silecs.info>
2012-11-10 21:43:34 +0100

    widget teachersel réorganisation code js

--- local/widget_teachersel/teachersel.js
+++ local/widget_teachersel/teachersel.js
@@ -16,50 +16,95 @@
     var cssUrl = $('script[src$="teachersel.js"]').attr('src').replace('/teachersel.js', '/teachersel.css');
     $('head').append($('<link rel="stylesheet" type="text/css" href=' + cssUrl + '>'));
 
-    var autocompleteUser = {
-        defaultSettings: {
+    var defaultSettings = {
             urlUsers: '../mwsteachers/service-search.php',
             minLength: 4,
             wsParams: { maxRows : 10 }, // default parameters for the web service
             inputSelector: 'input.user-selector', // class of the input field where completion takes place
             outputSelector: '.users-selected',
-        }
     };
 
     $.fn.autocompleteUser = function (options) {
-        var settings = $.extend(true, {}, autocompleteUser.defaultSettings, options || {});
+        var settings = $.extend(true, {}, defaultSettings, options || {});
+
         return this.each(function() {
             var $elem = $(this);
             var $input = $elem.find(settings.inputSelector).first();
-            $elem.addClass('autocomplete-user-select');
-            $input.on('click', function () {
+            var acg = new autocompleteUser(settings, $elem, $input);
+            acg.init();
+            acg.run();
+            $elem.data('autocompleteUser', acg);
+        });
+    }
+
+    function autocompleteUser(settings, elem, input) {
+        this.settings = settings;
+        this.elem = elem;
+        this.input = input;
+        return this;
+    }
+
+    autocompleteUser.prototype =
+    {
+        init: function() {
+            this.elem.addClass('autocomplete-user-select');
+            this.input.on('click', function () {
                 $(this).autocomplete("search");
             });
-            $input.autocomplete({
-                source: mainSource(settings),
+            $(this.settings.outputSelector, this.elem).on("click", ".selected-remove", function(event) {
+                var item = $(this).closest(".teacher-item-block").find('input[type="hidden"]').first();
+                selected[item.val()] = 0;
+                $(this).closest(".teacher-item-block").remove();
+            });
+        },
+
+        run: function() {
+            var $this = this;
+            $this.input.autocomplete({
+                source: $this.mainSource($this.settings),
                 select: function (event, ui) {
+
                     if($('#roleteacher').size()) {
 					    var inputName = $('#roleteacher').val();
 				    } else {
 					    var inputName = $(this).attr('data-inputname');
 				    }
+
                     if (ui.item.source == 'users') {
-                        $(settings.outputSelector, $elem).prepend(buildSelectedBlock(ui.item, inputName));
-                        $input.val('');
+                        $($this.settings.outputSelector, $this.elem)
+                            .prepend(buildSelectedBlock(ui.item, inputName));
                     }
                     return false;
                 },
                 open: function () {},
                 close: function () {},
-                minLength: settings.minLength
+                minLength: $this.settings.minLength
             }).data("autocomplete")._renderItem = customRenderItem;
+        },
 
-            $(settings.outputSelector, $elem).on("click", ".selected-remove", function(event) {
-                var item = $(this).closest(".teacher-item-block").find('input[type="hidden"]').first();
-                selected[item.val()] = 0;
-                $(this).closest(".teacher-item-block").remove();
-            });
-        });
+        mainSource: function() {
+            var $this = this;
+            return function(request, response) {
+                var wsParams = $.extend({}, $this.settings.wsParams);
+                wsParams.token = request.term;
+                wsParams.maxRows++;
+                $.ajax({
+                    url: $this.settings.urlUsers,
+                    dataType: "jsonp",
+                    data: wsParams,
+                    success: function (data) {
+                        response(
+                            $.merge(
+                                [{ label: "Utilisateurs", source: "title" }],
+                                $.map(data, function (item) {
+                                    return { label: item.displayName, value: item.uid, source: 'users' };
+                            }))
+                        );
+                    }
+                });
+
+            }
+        }
     }
 
     function customRenderItem(ul, item) {
@@ -71,27 +116,6 @@
             .append("<a><span>&oplus;</span>" + item.label + '</a>')
             .appendTo(ul);
     };
-
-    function mainSource(settings) {
-        return function(request, response) {
-            var wsParams = $.extend({}, settings.wsParams);
-            wsParams.token = request.term;
-            $.ajax({
-                url: settings.urlUsers,
-                dataType: "jsonp",
-                data: wsParams,
-                success: function (data) {
-                    response(
-                        $.merge(
-                            [{ label: "Utilisateurs", source: "title" }],
-                            $.map(data, function (item) {
-                                return { label: item.displayName, value: item.uid, source: 'users' };
-                         }))
-                    );
-                }
-            });
-        }
-    }
 
     function buildSelectedBlock(item, inputName) {
 		var role = inputName;
