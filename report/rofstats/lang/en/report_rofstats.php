ea975b44c6887dbe020cc6031aaa3c113b99f713
Guillaume Allegre <guillaume.allegre@silecs.info>
2013-01-08 19:56:28 +0100

    text strings for report_rofstats and report_up1stats

--- report/rofstats/lang/en/report_rofstats.php
+++ report/rofstats/lang/en/report_rofstats.php
@@ -4,9 +4,10 @@
  *
  * @package    report
  * @subpackage rofstats
- * @copyright  2012 Silecs {@link http://www.silecs.info/societe}
+ * @copyright  2012-2013 Silecs {@link http://www.silecs.info/societe}
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 $string['pluginname'] = 'ROF Statistics';
+$string['rofstats:view'] = 'View ROF Statistics';
 
