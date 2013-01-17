187eb78c3e56d7fe75485f91c4cb72c4523477cf
Guillaume Allegre <guillaume.allegre@silecs.info>
2013-01-17 21:43:43 +0100

    course_validated  index cosmetic enhancements

--- local/course_validated/index.php
+++ local/course_validated/index.php
@@ -30,15 +30,20 @@ $systemcontext   = get_context_instance(CONTEXT_SYSTEM);
 $PAGE->set_context($systemcontext);
 $PAGE->set_url('/local/course_validated/index.php');
 $PAGE->set_title("Espaces de cours en attente d'approbation");
+$PAGE->set_heading("Espaces de cours en attente d'approbation");
 
 echo $OUTPUT->header();
 
 // si l'utilisateur a la capacité supervalidator
 $table = get_table_course_to_validate(0, 2);
+// sinon
+// $table = get_table_course_to_validate($USER->id, 2);
 
 if (empty($table->data)) {
     echo $OUTPUT->box("Aucune demande de création de cours n'est en attente de validation.");
 } else {
+    $n = count(get_id_courses_to_validate(0, 0));
+    echo "<p>Il y a <b>" . $n . "</b> espaces de cours en attente d'approbation.</p>\n";
     $perpage = 20;
     $baseurl = new moodle_url('/local/course_validated/index.php');;
     $pagingbar = new paging_bar(count($table->data), $page, $perpage, $baseurl);
