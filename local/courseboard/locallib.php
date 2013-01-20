ef42e9e87fd6ac9ba949dbf3b0e5a32850ba46ef
Guillaume Allegre <guillaume.allegre@silecs.info>
2013-01-20 23:40:20 +0100

    courseboard categories + action icon edit metadata

--- local/courseboard/locallib.php
+++ local/courseboard/locallib.php
@@ -10,30 +10,31 @@ require_once($CFG->dirroot.'/course/lib.php');
 require_once($CFG->libdir.'/custominfo/lib.php');
 
 function table_course_vs_rof($crsid, $rofdata) {
+    global $DB, $OUTPUT;
 
     $crsfields = custominfo_data::type('course')->get_structured_fields_short($crsid, true);
-    echo "<ul>\n";
-    // var_dump($rofcourse);
-
     foreach ($crsfields as $category => $fields) {
         if ($category == 'Other fields') continue;
-            echo "<h4>" . $category . "</h4>\n";
+            $catid = $DB->get_field('custom_info_category', 'id', array('name' => $category));
+            $editurl = new moodle_url('/course/edit.php', array('id' => $crsid));
+            $editurl->set_anchor('category_' . $catid);
+            echo '<h4>' . $category . ' '
+                . $OUTPUT->action_icon($editurl, new pix_icon('t/edit', 'Modifier les métadonnées'))
+                . " </h4>\n";
 
             $table = new html_table();
-            // var_dump($rofdata[$category]);
-            // var_dump($fields);
             foreach ($fields as $shortname => $field ) {
                 $row = new html_table_row();
                 $row->cells[0] = new html_table_cell($field['name']);
                 $row->cells[0]->attributes = array('title' => $shortname, 'class' => '');
                 $row->cells[1] = $field['data'];
-                $row->cells[2] = (isset($rofdata[$category][$shortname]) ? $rofdata[$category][$shortname] : '(donnée absente)');
+                $row->cells[2] = (isset($rofdata[$category][$shortname]) ? $rofdata[$category][$shortname] : '(NA)');
                 $table->data[] = $row;
             }
             $table->data = array_merge(get_table_course_header(), $table->data);
             echo html_writer::table($table);
     } // categories
-    echo "</ul>\n";
+
 }
 
 
