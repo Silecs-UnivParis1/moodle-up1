af306557d18068afffcddeed6af8fdea4cd8aa5b
Guillaume Allegre <guillaume.allegre@silecs.info>
2013-01-22 16:05:02 +0100

    crswizard + settings (bug upgrade)

---
+++ local/crswizard/settings.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @package    local
+ * @subpackage crswizard
+ * @copyright  2012-2013 Silecs {@link http://www.silecs.info/societe}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die;
+require_once(dirname(dirname(dirname(__FILE__))) . '/config.php');
+require_once($CFG->libdir.'/adminlib.php');
+
+
+if ($ADMIN->fulltree) {
+    $settings->add(new admin_setting_configtext(
+            'local_crswizard_cohorts_cap_creator',
+            'Cohortes autorisées en création',
+            'Liste des cohortes autorisées à utiliser l\'assistant : identifiants séparés par des espaces.',
+            80,
+            PARAM_NOTAGS));
+
+    $settings->add(new admin_setting_configtext(
+            'local_crswizard_cohorts_cap_validator',
+            'Cohortes autorisées en approbation',
+            'Liste des cohortes autorisées à approuver les cours soumis via l\'assistant : identifiants séparés par des espaces.',
+            80,
+            PARAM_NOTAGS));
+}
