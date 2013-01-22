d4cdc74e8c8454b1e0b99a89ec6417727595ed36
François Gannaz <francois.gannaz@silecs.info>
2013-01-22 17:32:20 +0100

    CrsWizard: save settings locally

--- local/crswizard/settings.php
+++ local/crswizard/settings.php
@@ -6,24 +6,29 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+/* @var $ADMIN admin_root */
+
 defined('MOODLE_INTERNAL') || die;
-require_once(dirname(dirname(dirname(__FILE__))) . '/config.php');
-require_once($CFG->libdir.'/adminlib.php');
 
 if (has_capability('moodle/site:config', get_context_instance(CONTEXT_SYSTEM))) {
     $settings = new admin_settingpage('local_crswizard', 'Assistant de création de cours');
     $ADMIN->add('localplugins', $settings);
-    $settings->add(new admin_setting_configtext(
-            'crswizard_cohorts_cap_creator',
+
+    $cohorts_cap_creator = new admin_setting_configtext(
+            'cohorts_cap_creator',
             'Cohortes autorisées en création',
             'Liste des cohortes autorisées à utiliser l\'assistant : identifiants séparés par des espaces.',
             '',
-            PARAM_NOTAGS));
+            PARAM_NOTAGS);
+    $cohorts_cap_creator->plugin = 'local_crswizard';
+    $settings->add($cohorts_cap_creator);
 
-    $settings->add(new admin_setting_configtext(
-            'crswizard_cohorts_cap_validator',
+    $cohorts_cap_validator = new admin_setting_configtext(
+            'cohorts_cap_validator',
             'Cohortes autorisées en approbation',
             'Liste des cohortes autorisées à approuver les cours soumis via l\'assistant : identifiants séparés par des espaces.',
             '',
-            PARAM_NOTAGS));
+            PARAM_NOTAGS);
+    $cohorts_cap_validator->plugin = 'local_crswizard';
+    $settings->add($cohorts_cap_validator);
 }
