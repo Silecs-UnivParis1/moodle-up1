d51be1d9ccc8d7720f4e6aa05e4cdbddcf88bb10
François Gannaz <francois.gannaz@silecs.info>
2013-01-30 21:15:54 +0100

    mws search-users: add some unit tests

---
+++ local/mwsgroups/tests/mws_search_users_test.php
@@ -0,0 +1,144 @@
+<?php
+
+/*
+ * @license http://www.gnu.org/licenses/gpl-2.0.html  GNU GPL v2
+ */
+
+/**
+ * PHPUnit integration tests
+ *
+ * @package    local
+ * @subpackage mwsgroups
+ * @copyright  2012-2013 Silecs {@link http://www.silecs.info/societe}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once dirname(__DIR__) . '/lib.php';
+
+// To run only this test case: phpunit mws_search_users_testcase local/mwsgroups/tests/mws_search_users_test.php
+
+/**
+ * Test mws_search_users functions.
+ *
+ * @package    core
+ * @category   phpunit
+ * @copyright  2012 François Gannaz <francois.gannaz@silecs.info>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class mws_search_users_testcase extends advanced_testcase {
+    protected function setUp() {
+        parent::setUp();
+        static $init = false;
+        $this->resetAfterTest(false);
+        if (!$init) {
+            $dataset = $this->createCsvDataSet(
+                array(
+                    'user' => __DIR__ . '/fixtures/user.csv',
+                    'cohort' => __DIR__ . '/fixtures/cohort.csv',
+                    'cohort_members' => __DIR__ . '/fixtures/cohort_members.csv',
+                )
+            );
+            $this->loadDataSet($dataset);
+            $init = true;
+        }
+    }
+
+    public function test_simple_token() {
+        $search_u = new mws_search_users();
+        $search_u->maxrows = 10;
+        $search_u->filterstudent = 'both';
+        $search_u->exclude = array();
+        $search_u->affiliation = false;
+        $search_u->affectation = false;
+        $search_u->cohorts = array();
+
+        $users = $search_u->search("italo");
+        $this->assertInternalType('array', $users);
+        $this->assertCount(2, $users);
+
+        $this->assertCount(3, $search_u->search("i"));
+
+        $this->assertCount(8, $search_u->search("")); // including guest and admin
+
+        $this->assertCount(0, $search_u->search("féodor"));
+    }
+
+    public function test_filterstudent() {
+        $search_u = new mws_search_users();
+        $search_u->maxrows = 10;
+        $search_u->exclude = array();
+        $search_u->affiliation = false;
+        $search_u->affectation = false;
+        $search_u->cohorts = array();
+
+        $search_u->filterstudent = 'both';
+        $this->assertCount(3, $search_u->search("i"));
+
+        $search_u->filterstudent = 'only';
+        $this->assertCount(2, $search_u->search("i"));
+
+        $search_u->filterstudent = 'no';
+        $this->assertCount(1, $search_u->search("i"));
+
+        $search_u->filterstudent = 'no';
+        $this->assertCount(0, $search_u->search("italo"));
+    }
+
+    public function test_maxrows() {
+        $search_u = new mws_search_users();
+        $search_u->maxrows = 2;
+        $search_u->exclude = array();
+        $search_u->affiliation = false;
+        $search_u->affectation = false;
+        $search_u->cohorts = array();
+        $search_u->filterstudent = 'both';
+
+        $this->assertCount(2, $search_u->search("i"));
+    }
+
+    public function test_cohorts() {
+        $search_u = new mws_search_users();
+        $search_u->exclude = array();
+        $search_u->affiliation = false;
+        $search_u->affectation = false;
+        $search_u->filterstudent = 'both';
+
+        $search_u->cohorts = array('italiens');
+        $this->assertCount(2, $search_u->search("i"));
+
+        $search_u->cohorts = array('russes');
+        $this->assertCount(1, $search_u->search("i"));
+
+        $search_u->cohorts = array('none');
+        $this->assertCount(0, $search_u->search("i"));
+    }
+
+    public function test_exclude() {
+        $search_u = new mws_search_users();
+        $search_u->affiliation = false;
+        $search_u->affectation = false;
+        $search_u->filterstudent = 'both';
+        $search_u->cohorts = array();
+
+        $search_u->exclude = array('italo.svevo');
+        $this->assertCount(2, $search_u->search("i"));
+
+        $search_u->exclude = array('marcel.proust');
+        $this->assertCount(3, $search_u->search("i"));
+
+        $search_u->exclude = array('italo.svevo', 'italo.calvino');
+        $this->assertCount(1, $search_u->search("i"));
+
+        $search_u->maxrows = 2;
+        $search_u->exclude = array('italo.svevo', 'italo.calvino');
+        $this->assertCount(1, $search_u->search("i"));
+        $search_u->maxrows = 2;
+        $search_u->exclude = array('isaac.babel', 'italo.calvino');
+        $this->assertCount(1, $search_u->search("i"));
+        $search_u->maxrows = 2;
+        $search_u->exclude = array('isaac.babel', 'italo.svevo');
+        $this->assertCount(1, $search_u->search("i"));
+    }
+}
