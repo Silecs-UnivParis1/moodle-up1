363be3cf694d632cbbf2c9a5af650dd5d747596d
François Gannaz <francois.gannaz@silecs.info>
2013-01-31 11:45:25 +0100

    courseboard: code light cleanup

--- local/courseboard/locallib.php
+++ local/courseboard/locallib.php
@@ -1,43 +1,45 @@
 <?php
+
 /**
  * @package    local
  * @subpackage courseboard
  * @copyright  2012-2013 Silecs {@link http://www.silecs.info/societe}
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
+require_once($CFG->dirroot . '/course/lib.php');
+require_once($CFG->libdir . '/custominfo/lib.php');
 
-require_once($CFG->dirroot.'/course/lib.php');
-require_once($CFG->libdir.'/custominfo/lib.php');
+/* @var $DB moodle_database */
 
-function table_course_vs_rof($crsid, $rofdata) {
+function print_table_course_vs_rof($crsid, $rofdata) {
     global $DB, $OUTPUT;
 
     $crsfields = custominfo_data::type('course')->get_structured_fields_short($crsid, true);
     foreach ($crsfields as $category => $fields) {
-        if ($category == 'Other fields' || $category == 'Autres champs') continue;
-            $catid = $DB->get_field('custom_info_category', 'id', array('name' => $category));
-            $editurl = new moodle_url('/course/edit.php', array('id' => $crsid));
-            $editurl->set_anchor('category_' . $catid);
-            echo '<h4>' . $category . ' '
+        if ($category == 'Other fields' || $category == 'Autres champs') {
+            continue;
+        }
+        $catid = $DB->get_field('custom_info_category', 'id', array('name' => $category));
+        $editurl = new moodle_url('/course/edit.php', array('id' => $crsid));
+        $editurl->set_anchor('category_' . $catid);
+        echo '<h4>' . $category . ' '
                 . $OUTPUT->action_icon($editurl, new pix_icon('t/edit', 'Modifier les métadonnées'))
                 . " </h4>\n";
 
-            $table = new html_table();
-            $table->data=array();
-            foreach ($fields as $shortname => $field ) {
-                $row = new html_table_row();
-                $row->cells[0] = new html_table_cell($field['name']);
-                $row->cells[0]->attributes = array('title' => $shortname, 'class' => '');
-                $row->cells[1] = $field['data'];
-                $row->cells[2] = (isset($rofdata[$category][$shortname]) ? $rofdata[$category][$shortname] : '(NA)');
-                $table->data[] = $row;
-            }
-            $table->data = array_merge(get_table_course_header(), $table->data);
-            echo html_writer::table($table);
+        $table = new html_table();
+        $table->data = array();
+        foreach ($fields as $shortname => $field) {
+            $row = new html_table_row();
+            $row->cells[0] = new html_table_cell($field['name']);
+            $row->cells[0]->attributes = array('title' => $shortname, 'class' => '');
+            $row->cells[1] = $field['data'];
+            $row->cells[2] = (isset($rofdata[$category][$shortname]) ? $rofdata[$category][$shortname] : '(NA)');
+            $table->data[] = $row;
+        }
+        $table->data = array_merge(get_table_course_header(), $table->data);
+        echo html_writer::table($table);
     } // categories
-
 }
-
 
 function get_table_course_header() {
     $headings = array('Métadonnée', 'Cours', 'ROF');
@@ -59,7 +61,7 @@ function print_admin_log($crsid) {
     global $DB;
 
     $table = new html_table();
-    $table->classes = array('logtable','generalbox');
+    $table->classes = array('logtable', 'generalbox');
     $table->align = array('right', 'left', 'left');
     $table->head = array(
         get_string('time'),
@@ -70,19 +72,18 @@ function print_admin_log($crsid) {
     $table->data = array();
 
     $sql = "SELECT l.id, time, userid, ip, module, action, l.url, info, u.firstname, u.lastname "
-         . "FROM {log} l JOIN {user} u  ON (l.userid = u.id)"
-         . "WHERE ( ( module = 'course' AND action = 'new' AND info LIKE '%ID ".$crsid."%' ) "
-         . " OR ( module = 'course' AND action != 'view' AND action != 'login' AND course = ? ) "
-         . " OR (module IN ('course_validate', 'crswizard') AND course = ?)  ) "
-         . " ORDER BY time ASC ";
+            . "FROM {log} l JOIN {user} u  ON (l.userid = u.id)"
+            . "WHERE ( ( module = 'course' AND action = 'new' AND info LIKE '%ID " . $crsid . "%' ) "
+            . " OR ( module = 'course' AND action != 'view' AND action != 'login' AND course = ? ) "
+            . " OR (module IN ('course_validate', 'crswizard') AND course = ?)  ) "
+            . " ORDER BY time ASC ";
     $logs = $DB->get_records_sql($sql, array($crsid, $crsid));
 
-    foreach($logs as $log) {
+    foreach ($logs as $log) {
         $row = new html_table_row();
-
         $row->cells[0] = new html_table_cell(userdate($log->time, '%Y-%m-%d %H:%M:%S'));
         $row->cells[1] = new html_table_cell($log->firstname . ' ' . $log->lastname);
-        $row->cells[2] = new html_table_cell($log->module .' '. $log->action);
+        $row->cells[2] = new html_table_cell($log->module . ' ' . $log->action);
         $row->cells[3] = new html_table_cell($log->info);
         $table->data[] = $row;
     }
--- local/courseboard/view.php
+++ local/courseboard/view.php
@@ -1,17 +1,19 @@
 <?php
+
 /**
  * @package    local
  * @subpackage courseboard
  * @copyright  2012-2013 Silecs {@link http://www.silecs.info/societe}
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-
 define('NO_OUTPUT_BUFFERING', false);
-global $DB;
+
+global $CFG, $DB, $PAGE, $OUTPUT;
+
 require('../../config.php');
-require_once($CFG->dirroot.'/local/roftools/roflib.php'); // to get ROF data
-require_once($CFG->dirroot.'/local/up1_metadata/lib.php'); // to get prepared metadata
-require_once($CFG->libdir.'/adminlib.php');
+require_once($CFG->dirroot . '/local/roftools/roflib.php'); // to get ROF data
+require_once($CFG->dirroot . '/local/up1_metadata/lib.php'); // to get prepared metadata
+require_once($CFG->libdir . '/adminlib.php');
 require_once('locallib.php');
 
 require_login();
@@ -24,48 +26,49 @@ require_capability('moodle/site:approvecourse', $context_sys); //** @todo trouve
 $PAGE->set_context($context_sys);
 $PAGE->set_url('/local/courseboard/view.php', array('id' => $crsid));
 $PAGE->set_pagelayout('course'); //** @todo nécessaire ici ?
-$PAGE->set_title($course->shortname .': '. "tableau de bord");
-$PAGE->set_heading($course->fullname .' : '. "tableau de bord");
+$PAGE->set_title($course->shortname . ': ' . "tableau de bord");
+$PAGE->set_heading($course->fullname . ' : ' . "tableau de bord");
 
 echo $OUTPUT->header();
-echo $OUTPUT->heading('Tableau de bord ' . $course->shortname );
+echo $OUTPUT->heading('Tableau de bord ' . $course->shortname);
 
 // $url = "$CFG->wwwroot/local/courseboard/view.php";
 $browserurl = "$CFG->wwwroot/local/rof_browser/rof_browser.php";
 
 echo "<h2>Description</h2>\n";
 echo "<ul>\n";
-echo "<li>Nom complet : ". $course->fullname ."</li>\n";
-echo "<li>Nom abrégé : ". $course->shortname ."</li>\n";
-echo "<li>No identification : ". ( ! empty($course->idnumber) ? $course->idnumber : '<b>NON renseigné</b>') ."</li>\n";
+echo "<li>Nom complet : " . $course->fullname . "</li>\n";
+echo "<li>Nom abrégé : " . $course->shortname . "</li>\n";
+echo "<li>No identification : " . (!empty($course->idnumber) ? $course->idnumber : '<b>NON renseigné</b>') . "</li>\n";
 echo "</ul>\n";
 
-    // ROF data
-    if ( $rofid = up1_meta_get_text($crsid, 'up1rofid') ) {
-        $rofcourse = $DB->get_record('rof_course', array('rofid' => $rofid));
-    } else {
-        $rofcourse = FALSE;
-        $rofid = FALSE;
-        echo "<h3>Pas de ROF</h3>\n";
-        echo "<p>Aucun cours correspondant n'existe dans le ROF avec ce code <b>$course->idnumber</b>.</p>\n";
+// ROF data
+$rofid = up1_meta_get_text($crsid, 'up1rofid');
+if ($rofid) {
+    $rofcourse = $DB->get_record('rof_course', array('rofid' => $rofid));
+} else {
+    $rofcourse = FALSE;
+    $rofid = FALSE;
+    echo "<h3>Pas de ROF</h3>\n";
+    echo "<p>Aucun cours correspondant n'existe dans le ROF avec ce code <b>$course->idnumber</b>.</p>\n";
+}
+
+$rofdata = rof_get_metadata($rofid);
+print_table_course_vs_rof($crsid, $rofdata);
+
+if ($rofcourse) {
+    echo "<h3>Tous les chemins : </h3>\n";
+    $allPaths = rof_get_course_all_paths($rofcourse->rofid);
+    $allPathnames = rof_get_course_all_pathnames($allPaths);
+    echo '<ol>';
+    foreach ($allPathnames as $pathname) {
+        echo '<li>' . rof_format_path($pathname, 'combined', true) . '</li>';
     }
+    echo '</ol>';
+} else {
 
-    $rofdata = rof_get_metadata($rofid);
-    $table = table_course_vs_rof($crsid, $rofdata);
-
-    if ( $rofcourse ) {
-        echo "<h3>Tous les chemins : </h3>\n";
-        $allPaths = rof_get_course_all_paths($rofcourse->rofid);
-        $allPathnames = rof_get_course_all_pathnames($allPaths);
-        echo '<ol>';
-        foreach ($allPathnames as $pathname) {
-            echo '<li>' . rof_format_path($pathname, 'combined', true) . '</li>';
-        }
-        echo '</ol>';
-    } else {
-
-    }
-echo '<p><a href="' . $browserurl. '">Navigateur ROF</a></p>';
+}
+echo '<p><a href="' . $browserurl . '">Navigateur ROF</a></p>';
 
 print_admin_log($crsid);
 
