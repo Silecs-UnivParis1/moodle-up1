88dbb9eb835538079aee3718254d864734b5233f
Guillaume Allegre <guillaume.allegre@silecs.info>
2013-02-15 15:53:29 +0100

    local_mwscoursetree draft

---
+++ local/mwscoursetree/sample.php
@@ -0,0 +1,22 @@
+<?php
+
+$sample = array(
+    0 => array(
+        'name' => 'Tutorat Méthodologie',
+        'courselink' => 'http://localhost/moodle-paris1/course/view.php?id=26',
+        'courses' => 12,
+        'children' => true,
+        'teachers' => array('Maxime Dupuis', 'Patricia Cavallo', 'Benoit Roques'),
+        'synopsis' => 'http://localhost/moodle-paris1/course/report/synopsis/index.php?id=20',
+        'access' => array('self-enrolment'),
+    ),
+    1 => array(
+        'name' => '0210305 - Mathématique1',
+        'courselink' => null,
+        'courses' => 7,
+        'children' => true,
+        'teachers' => array(),
+        'synopsis' => null,
+        'access' => array(),
+    )
+);
