64998f2400a62526a2212a057e16ce7ad5d45f32
Cécile Guilloux <cecile.guilloux@silecs.info>
2013-02-17 17:57:35 +0100

    crswizard allégé - ajout lien menu Navigation (assisitant création) du cours

--- local/crswizard/lib.php
+++ local/crswizard/lib.php
@@ -11,10 +11,15 @@ require_once(__DIR__ . '/libaccess.php');
 
 // doc https://moodle.org/mod/forum/discuss.php?d=170325#yui_3_7_3_2_1359043225921_310
 function crswizard_extends_navigation(global_navigation $navigation) {
-    global $USER;
+    global $USER, $PAGE;
 
     $permcreator = wizard_has_permission('creator', $USER->id);
     $permvalidator = wizard_has_permission('validator', $USER->id);
+    $permassistant = false;
+    $context = $PAGE->context;
+    if ($context->contextlevel == 50 && $context->instanceid != 1) {
+        $permassistant = wizard_update_has_permission($context->instanceid, $USER->id);
+    }
 
     if ($permcreator || $permvalidator) {
         $node1 = $navigation->add('Assistant création de cours');
@@ -24,7 +29,12 @@ function crswizard_extends_navigation(global_navigation $navigation) {
         if ($permvalidator) {
             $node3 = $node1->add('Approbation', new moodle_url('/local/course_validated/index.php'));
         }
+        if ($permassistant) {
+            $node3 = $node1->add('Assistant paramétrage', new moodle_url('/local/crswizard/update/index.php',
+                array('id' => $context->instanceid)));
+        }
+    } elseif ($permassistant) {
+        $node1 = $navigation->add('Assistant paramétrage', new moodle_url('/local/crswizard/update/index.php',
+                array('id' => $context->instanceid)));
     }
 }
-
-
