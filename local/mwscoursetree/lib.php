729f383cb559709b005feb292bc3afc4fd43df0e
Guillaume Allegre <guillaume.allegre@silecs.info>
2013-02-17 17:13:43 +0100

    mwscoursetree get_children() complete draft

--- local/mwscoursetree/lib.php
+++ local/mwscoursetree/lib.php
@@ -8,6 +8,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 require_once($CFG->dirroot . "/local/up1_metadata/lib.php");
+require_once($CFG->dirroot . "/local/roftools/roflib.php");
 
 function get_children($node) {
     global $DB;
@@ -43,27 +44,29 @@ function get_children($node) {
         } elseif ($parentcat->depth == 4) { // CASE 2 node=category and children = ROF entries or courses
             $courses = get_courses($parentcatid, "c.sortorder ASC", "c.id");
             list($rofcourses, $catcourses) = split_courses_from_rof($courses);
-//var_dump($rofcourses);
-//var_dump($catcourses);
-//die();
             foreach ($catcourses as $crsid) {
                 $result[] = get_entry_from_course($crsid, 5);
             }
-            foreach ($rofcourses as $course) {
-                //
-            }
+            $result = array_merge($result, get_entries_from_rof_courses($rofcourses, 5, $pseudopath, $parentcatid));
         } else {
             throw new coding_exception('Category depth should not be > 4.');
         }
 
-    } else { // under ROF root
-
+    } else { // CASE 3 under ROF root
+        $rofpath = '/' . join('/', array_slice($pseudopath, 1));
+        $depth = 3 + count($pseudopath);
+//var_dump($rofpath);
+        $fieldid = $DB->get_field('custom_info_field', 'id',
+                array('objectname' => 'course', 'shortname' => 'up1rofpathid'), MUST_EXIST);
+        $sql = "SELECT objectid, data FROM {custom_info_data} "
+             . "WHERE objectname='course' AND fieldid=? AND data LIKE ?";
+        $rofcourses = $DB->get_records_sql_menu($sql, array($fieldid, '%'.$rofpath.'%'));
+//var_dump($rofcourses);
+        $result = get_entries_from_rof_courses($rofcourses, $depth, $pseudopath, $parentcatid);
     }
 
     return $result;
 }
-
-
 
 function get_entry_from_course($crsid, $depth) {
     global $DB;
@@ -82,7 +85,7 @@ function split_courses_from_rof($courses) {
     $rofcourses = array();
     $catcourses = array();
     foreach($courses as $course) {
-        $rofpathid = up1_meta_get_text($course->id, 'rofpathid', $error=false);
+        $rofpathid = up1_meta_get_text($course->id, 'rofpathid', false);
         if ($rofpathid) {
             $rofcourses[$course->id] = $rofpathid;
         } else {
@@ -90,4 +93,51 @@ function split_courses_from_rof($courses) {
         }
     }
     return array($rofcourses, $catcourses);
+}
+
+function get_component_from_category($catid) {
+    global $DB;
+    $idnumber = $DB->get_field('course_categories', 'idnumber', array('id' => $catid), MUST_EXIST);
+    return substr($idnumber, 2, 2); // ex. '4:05/Masters' -> '05'
+}
+
+function get_entries_from_rof_courses($rofcourses, $depth, $pseudopath, $parentcatid) {
+    $component = get_component_from_category($parentcatid);
+    $prenodes = array();
+    $items = array();
+
+    foreach ($rofcourses as $crsid => $dummy) {
+        $rofpathids = array_filter(explode(';', up1_meta_get_text($crsid, 'rofpathid', false)));
+        foreach ($rofpathids as $rofpathid) {
+            $arrofpath = array_filter(explode('/', $rofpathid));
+            $prenode = "/$parentcatid" . '/' . join('/', array_slice($arrofpath, 0, $depth - 3));
+            if (count($arrofpath) == $depth - 3 ) { // leaf
+                $directcourse[$prenode] = $crsid;
+            } elseif (count($arrofpath) > $depth - 3 ) { // subfolders
+                $unfold[$prenode] = true;
+            }
+            $prenodes[] = $prenode;
+        }
+    }
+// var_dump($prenodes);
+    foreach (array_unique($prenodes) as $node) {
+        $arrofpath = explode('/', $node);
+        $rofid = array_pop($arrofpath);
+        list($rofobject, $top) = rof_get_record($rofid);
+        $name = $rofobject->name;
+
+        if ( isset($directcourse[$node]) &&  $directcourse[$node] ) {
+            $url = new moodle_url('/course/view.php', array('id' => $directcourse[$node]));
+            $item['label'] = html_writer::link($url, $name);
+        } else {
+            $item['label'] = $name;
+        }
+        if ( isset($unfold[$node]) &&  $unfold[$node] ) {
+            $item['load_on_demand'] = true;
+        }
+        $item['node'] = $node;
+        $item['depth'] = $depth;
+        $items[] = $item;
+    }
+    return $items;
 }
