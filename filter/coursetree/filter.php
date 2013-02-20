10931a1bc0abf3ed76770e0e71d459e6afcfba35
Guillaume Allegre <guillaume.allegre@silecs.info>
2013-02-21 00:25:57 +0100

    + filter_coursetree

---
+++ filter/coursetree/filter.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @package    filter
+ * @subpackage coursetree
+ * @copyright  2013 Silecs {@link http://www.silecs.info/societe}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+class filter_coursetree extends moodle_text_filter {
+
+    public function filter($text, array $options = array()) {
+        if ( preg_match('/\[coursetree (.+)\]/', $text, $matches) ) {
+                $widget =  new moodle_url('/local/mwscoursetree/widget.js');
+                $script = '<script type="text/javascript" src="' . $widget . '"></script>';
+                $div = '<div class="coursetree" data-root="' . $matches[1] .'"></div>';
+
+            //$replace = 'coursetree ' . print_r($options, true);
+            $replace = $script . $div;
+            return str_replace($matches[0], $replace, $text);
+        } else {
+            return $text;
+        }
+
+    }
+
+}
