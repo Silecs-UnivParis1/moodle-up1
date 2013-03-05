24aaf75404e2fa5eff651b2bbb59fb10cf8c076b
François Gannaz <francois.gannaz@silecs.info>
2013-03-05 10:45:37 +0100

    M#1176: fix coursetreefilter for multiple inserts

--- filter/coursetree/filter.php
+++ filter/coursetree/filter.php
@@ -11,18 +11,17 @@ defined('MOODLE_INTERNAL') || die();
 class filter_coursetree extends moodle_text_filter {
 
     public function filter($text, array $options = array()) {
-        if ( preg_match( '#\[coursetree (/[\d/]+[\w/-]*)\]#', $text, $matches) ) {
-                $widget =  new moodle_url('/local/mwscoursetree/widget.js');
-                $script = '<script type="text/javascript" src="' . $widget . '"></script>';
-                $div = '<div class="coursetree" data-root="' . $matches[1] .'"></div>';
+        while ( preg_match( '#\[coursetree (/[\d/]+[\w/-]*)\]#', $text, $matches) ) {
+            $widget =  new moodle_url('/local/mwscoursetree/widget.js');
+            $script = '<script type="text/javascript" src="' . $widget . '"></script>';
+            $div = '<div class="coursetree" data-root="' . $matches[1] .'"></div>';
 
             //$replace = 'coursetree ' . print_r($options, true);
             $replace = $script . $div;
-            return str_replace($matches[0], $replace, $text);
-        } else {
-            return $text;
+            $text = str_replace($matches[0], $replace, $text);
         }
 
+        return $text;
     }
 
 }
