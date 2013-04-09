7d697d82b607280857b19ebc5c750fe2a04fa8cc
Cécile Guilloux <cecile.guilloux@silecs.info>
2013-04-09 15:22:26 +0200

    ajout nouvelle capacité (nouveau module) : local/up1_capabilities:course_updatesettings

---
+++ local/up1_capabilities/lang/en/local_up1_capabilities.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Version details
+ *
+ * @package    local
+ * @subpackage up1_capabilities
+ * @copyright  2012 Silecs {@link http://www.silecs.info/societe}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'Capacités UP1';
+$string['up1_capabilities:course_updatesettings'] = 'Accéder à la page Settings du cours';
+
+
