2a8e6f6eef21a560fe3014919937d0a9121a223e
Guillaume Allegre <guillaume.allegre@silecs.info>
2013-04-17 10:00:25 +0200

    + local_up1_courselist

---
+++ local/up1_courselist/db/access.php
@@ -0,0 +1,11 @@
+<?php
+// This file is part of a plugin for Moodle - http://moodle.org/
+
+/**
+ * @package    local
+ * @subpackage up1_courselist
+ * @copyright  2013 Silecs {@link http://www.silecs.info/societe}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
---
+++ local/up1_courselist/version.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @package    local
+ * @subpackage up1_courselist
+ * @copyright  2013 Silecs {@link http://www.silecs.info/societe}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version   = 2013041700;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->requires  = 2012061700;        // Requires this Moodle version
+$plugin->component = 'local_up1_courselist';       // Full name of the plugin (used for diagnostics)
+
+$plugin->cron      = 0;
+$plugin->maturity  = MATURITY_BETA;
+$plugin->release   = 'TODO';
+ 
+$plugin->dependencies = array();
