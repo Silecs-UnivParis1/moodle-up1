787aadc93d0c113f889d185d346572c591a5ea36
Guillaume Allegre <guillaume.allegre@silecs.info>
2013-04-17 10:00:25 +0200

    + local_up1_courselist

---
+++ local/up1_courselist/lang/en/local_up1_courselist.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Version details
+ *
+ * @package    local
+ * @subpackage up1_courselist
+ * @copyright  2013 Silecs {@link http://www.silecs.info/societe}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'Liste des cours UP1';
+
+
