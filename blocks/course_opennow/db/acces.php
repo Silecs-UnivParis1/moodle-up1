e2dea12f3f614b0e6d853a58b80ae315f5425953
Guillaume Allegre <guillaume.allegre@silecs.info>
2013-04-24 16:35:30 +0200

    block_course_opennow + block/course_opennow:addinstance

---
+++ blocks/course_opennow/db/acces.php
@@ -0,0 +1,26 @@
+<?php
+// This file is part of a plugin for Moodle - http://moodle.org/
+
+/**
+ * @package    block
+ * @subpackage course_opennow
+ * @copyright  2012-2013 Silecs {@link http://www.silecs.info/societe}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$capabilities = array(
+
+    'block/course_opennow:addinstance' => array(
+        'riskbitmask' => RISK_CONFIG,
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_BLOCK,
+        'archetypes' => array(
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        ),
+
+        'clonepermissionsfrom' => 'moodle/site:manageblocks'
+    ),
+);
--- blocks/course_opennow/version.php
+++ blocks/course_opennow/version.php
@@ -4,12 +4,12 @@
 /**
  * @package    block
  * @subpackage course_opennow
- * @copyright  2012 Silecs {@link http://www.silecs.info/societe}
+ * @copyright  2012-2013 Silecs {@link http://www.silecs.info/societe}
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2012101100;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2013042402;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2012061700;        // Requires this Moodle version
 $plugin->component = 'block_course_opennow';       // Full name of the plugin (used for diagnostics)
