3093a4c200a170c41ab46afc3e7850d693caaf50
François Gannaz <francois.gannaz@silecs.info>
2013-05-15 19:15:11 +0200

    Fake jquery module version

---
+++ local/jquery/version.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Version details
+ */
+
+$plugin->release   = '5.0';
+$plugin->version   = 2015010900;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->requires  = 2012112900;        // Requires this Moodle version
+$plugin->component = 'local_jquery';
+
