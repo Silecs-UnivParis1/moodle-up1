25fd1d14e154c43016577df67d96812f243c9944
Guillaume Allegre <guillaume.allegre@silecs.info>
2013-05-30 00:13:59 +0200

    rof_sync + db upgrade for localnames

--- local/rof_sync/version.php
+++ local/rof_sync/version.php
@@ -4,12 +4,12 @@
 /**
  * @package    local
  * @subpackage rof_sync
- * @copyright  2012 Silecs {@link http://www.silecs.info/societe}
+ * @copyright  2012-2013 Silecs {@link http://www.silecs.info/societe}
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2012092701;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2013052903;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2012061700;        // Requires this Moodle version
 $plugin->component = 'local_rof_sync';       // Full name of the plugin (used for diagnostics)
