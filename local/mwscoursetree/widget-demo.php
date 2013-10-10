5e7704ebb02498a248b42425e221e2e27a91b628
Guillaume Allegre <guillaume.allegre@silecs.info>
2013-10-10 08:52:00 +0200

    local_mwscoursetree widget_demo.php + node param

--- local/mwscoursetree/widget-demo.php
+++ local/mwscoursetree/widget-demo.php
@@ -6,18 +6,22 @@ global $PAGE, $OUTPUT;
 
 require_login();
 
+$node = optional_param('node', '/cat0', PARAM_RAW);
+
 $PAGE->set_context(get_context_instance(CONTEXT_SYSTEM));
 $PAGE->set_url('/local/mwscoursetree/widget-demo.php');
 $PAGE->set_title("Démo de l'arbre des cours");
-
 $PAGE->requires->js(new moodle_url('/local/mwscoursetree/widget.js'), true);
-
 $PAGE->set_pagelayout('admin');
 
 echo $OUTPUT->header();
 echo $OUTPUT->heading("Démo de l'arbre des cours");
 
+echo "<p>Cette page affiche par défaut un arbre partant de la racine <b>/cat0</b>.";
+echo "Vous pouvez modifier la racine en ajoutant à l'URL <b>?node=/cat1</b> par exemple. </p>";
+
 ?>
+
 
 <style type="text/css">
 .jqtree-hidden {
@@ -26,7 +30,9 @@ echo $OUTPUT->heading("Démo de l'arbre des cours");
 </style>
 
 <script type="text/javascript" src="<?php echo new moodle_url('/local/mwscoursetree/widget.js'); ?>"></script>
-<div class="coursetree" data-root="/cat0" data-title="1"></div>
+<!--div class="coursetree" data-root="</cat0" data-title="1"></div-->
+<div class="coursetree" data-root="<?php echo $node; ?>" data-title="1"></div>
+
 <?php
 
 echo $OUTPUT->footer();
