a9133e5731a3a2e82c66a30cc036a877fa4ec4cf
Guillaume Allegre <guillaume.allegre@silecs.info>
2013-12-19 19:58:58 +0100

    M1204#c7035 ébauche reporting administrateur : totalisation par UFR

---
+++ admin/tool/up1_reporting/lang/fr/tool_up1_reporting.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Administrator reporting
+ *
+ * @package    tool
+ * @subpackage up1_reporting
+ * @copyright  2013 Silecs {@link http://www.silecs.info/societe}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'UP1 Reporting administrateur';
